13cc6a0970eaa150b9eca0ac1fe4f554fea6d2f1
vaejiang19960307@gmail.com <vaejiang19960307@gmail.com>
2023-08-01 12:47:54 +0000

    add content

---
+++ 1 - Foundations of Project Management.py
@@ -0,0 +1,221 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC
+# MAGIC # Foundations of Project Management
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC
+# MAGIC ### Learning Objectives
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC - Relate the skills and responsibilities of a project manager necessary to achieve success on the job.
+# MAGIC - Define project management and describe what constitutes a project.
+# MAGIC - Explain project management roles and responsibilities across a variety of industries.
+# MAGIC - Detail and relate project management skills to project management and its value to business.
+# MAGIC - Determine how this program will help prepare you for a career in project management.
+# MAGIC - Explain the Project Management certificate program structure and course functionality.
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC
+# MAGIC ### What is Project Management
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC
+# MAGIC #### Definition of Project Management
+# MAGIC - The application of knowledge, skill, tool and techniques to meet project requirement and acheive desirable outcome
+# MAGIC
+# MAGIC #### Definition of Project
+# MAGIC - It is a unique endevor which contains a set of unique deliverable
+# MAGIC - it is a temporary pursuit with a beginning date and end date
+# MAGIC
+# MAGIC #### Why Project Management is Important
+# MAGIC - Poor PM can leads project miss delivery date, miss target budget, not meet organization's goal
+# MAGIC
+# MAGIC #### Project Manager vs Programm Manager vs Profolio Manager
+# MAGIC - `Project Manager`: manage one specific project
+# MAGIC - `Program Manager`: Manage a range of similar projects and program managers; they facilitate and communicate with project manager. 
+# MAGIC - `Profolio Manager`: Manager a group of related program within organization. 
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC
+# MAGIC ### What is Project Manager 
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC
+# MAGIC #### Project Manager Mission Statement
+# MAGIC - Shepherd Project From start to finish and serve as a `guild` for team members with `interpersonal` and `organizational` skills
+# MAGIC
+# MAGIC #### Key Skills
+# MAGIC - Prioritization 
+# MAGIC   - Identify and break down projects into small tasks
+# MAGIC   - connect with stakeholders and team to gather information and identify most pressing tasks
+# MAGIC - Delegation 
+# MAGIC   - Assing tasks to individual who suits the tasks (*You need know your team*)
+# MAGIC - Effective Communication
+# MAGIC   - **Being Upfront about plan and ideas **
+# MAGIC   - **make information readily avaliable**
+# MAGIC   - **Make sure you understands customer's expectation**
+# MAGIC   - **Ensure each team member understands task goals and team goals**
+# MAGIC
+# MAGIC #### Who is Customers
+# MAGIC - a person or an organization that defines the requirements of the project and sets important guidelines
+# MAGIC - **Taking the time to dig a little deeper into the “why” behind the project can help a project manager better support and understand the customer.**
+# MAGIC
+# MAGIC #### What Project Manager Needs
+# MAGIC - take the time to build relationships, communicate, and treat others with consideration and respect.
+# MAGIC - **Be Flexible**: Project changes all the time and it is important to be flexible to change
+# MAGIC   - Keep external factors in plan such as national holiday or team member vacation
+# MAGIC   - Plan for risk: ie, if someone from your team quit, how will you find replacement
+# MAGIC   - Calculate Float: the amount of time you can wait before it impact the project outcome
+# MAGIC - **Strong Organizational Skills**
+# MAGIC - **enabling decision making**
+# MAGIC - influncing without authority
+# MAGIC
+# MAGIC #### What Prooject Manager Does
+# MAGIC - Planning and Organizing
+# MAGIC   - Gathering Requirement from Teammate and Customer
+# MAGIC   - **You should understand what your project is trying to accompalish**
+# MAGIC   - Create project plan
+# MAGIC   - Communicate team progress to stakeholders
+# MAGIC - Manging Task
+# MAGIC - Budgeting
+# MAGIC   - **Keep Project on track and in budget**
+# MAGIC   - monitor and manage budget
+# MAGIC   - track issue and risks (`escalate the risk to the right people at the right time`)
+# MAGIC   - manage quality
+# MAGIC   - removing unforseeable barrier
+# MAGIC - Controlling Cost and Other Factors
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC
+# MAGIC ### Project Life Cycle
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC
+# MAGIC #### Four Main Project Life Cycle
+# MAGIC - Initialize project 
+# MAGIC   - Identify project goal and deliverable 
+# MAGIC   - Identify budget 
+# MAGIC   - Personnels involve in project
+# MAGIC - Make a plan 
+# MAGIC - Execute and complete the plan 
+# MAGIC   - Monitor project 
+# MAGIC   - Keep team motivated
+# MAGIC - close the project
+# MAGIC
+# MAGIC #### Initialize project 
+# MAGIC - Identify all avaliable information to `define project goal` which makes your project detail clear
+# MAGIC - Determine what resource, people, and info are avaliable (anything you need to complete a project)
+# MAGIC - Get approval from decision maker
+# MAGIC - `Questions to ask`
+# MAGIC   - Who are the stakeholders?
+# MAGIC   - What are the client’s or customer’s goals?
+# MAGIC   - What is the purpose and mission of the project?
+# MAGIC   - What are the measurable objectives for the team?
+# MAGIC   - What is the project trying to improve? 
+# MAGIC   - When does this project need to be completed? 
+# MAGIC   - What skills and resources will the project require? 
+# MAGIC   - What will the project cost? What are the benefits?
+# MAGIC
+# MAGIC #### Make a plan 
+# MAGIC - Create Budget
+# MAGIC - Set project timeline
+# MAGIC - Establish project team
+# MAGIC - Determine each person's role and responsbility
+# MAGIC - **Plan risk and changes**
+# MAGIC - Establish Communication
+# MAGIC - `Questions to ask`
+# MAGIC   - What are the major milestones? What tasks or deliverables make up each milestone?  
+# MAGIC   - Build out the schedule so you can properly manage the resources, budget, materials, and timeline. Here, you will create an itemized budget.
+# MAGIC
+# MAGIC #### Execute and complete the plan 
+# MAGIC - **Your job as project manager is not do tasks but manage the process**
+# MAGIC - Remove Obsticales
+# MAGIC - Communicate with your team and stakeholder via reports, email, etc. (**Overcommmunicating is a good practice**)
+# MAGIC - `Questions to ask`
+# MAGIC
+# MAGIC #### close the project
+# MAGIC - Ensure all tasks have been completed
+# MAGIC - Confirm acceptance of project outcome
+# MAGIC - Confirm project outcome is accepted
+# MAGIC - Communicate final results to stakeholders
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC
+# MAGIC ### Project Methodologies
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC
+# MAGIC #### Linear Project Method - Waterfall
+# MAGIC - `Meaning`: the second task of the project can start when the first one is completed
+# MAGIC - *it is suitable for project like building a house*
+# MAGIC - The end result of project is predefined and it cannot change
+# MAGIC - Waterfall approach has risk management tools to help dealing with project scope change
+# MAGIC
+# MAGIC #### Iterative Project Method - Agile
+# MAGIC - `Meaning`: Tasks can happen at the same time while other tasks has been working on
+# MAGIC - This method is flexible and end result can always change
+# MAGIC - The agile approach starts in 90s when demand for fast delivery is asked
+# MAGIC - Stakeholders have an idea what they want but don't have a solid picture yet
+# MAGIC
+# MAGIC #### Lean Six Sigma Method
+# MAGIC - **This approach most used when you need identify the issue and find ways for improvement**
+# MAGIC - `Lead Approach`
+# MAGIC   - it is used to reduce cost / waste, limit resource, and streamline processes
+# MAGIC   - Recognize eight types of waste (defects, non-utilized talent, excess processing, overproduction, inventory, transportation, wait)
+# MAGIC   - It uses `5S` to address those wastes: 
+# MAGIC     - Sort: Remove all items not needed for current production operations and leave only the bare essentials. 
+# MAGIC     - Set in order: Arrange needed items so that they are easy to use. Label items so that anyone can find them or put them away. 
+# MAGIC     - Shine: Keep everything in the correct place. Clean your workspace every day.
+# MAGIC     - Standardize: Perform the process in the same way every time.
+# MAGIC     - Sustain: Make a habit of maintaining correct procedures and instill this discipline in your team.
+# MAGIC - `Six Sigma Approach`
+# MAGIC   - it is used to ensure quality process is followed everytimes
+# MAGIC   - Identify measurable KPI such as time, cost, and time spent and remove those don't match `six sigma` standard
+# MAGIC     - Always focus on the customer.
+# MAGIC     - Identify and understand how the work gets done. Understand how work really happens.
+# MAGIC     - Make your processes flow smoothly.
+# MAGIC     - Reduce waste and concentrate on value.
+# MAGIC     - Stop defects by removing variation.
+# MAGIC     - Involve and collaborate with your team.
+# MAGIC     - Approach improvement activity in a systematic way.
+# MAGIC - Lean Six Sigma Stages 
+# MAGIC   - Define: Talk with stakeholders and find out issues
+# MAGIC   - Measure: Collect data
+# MAGIC   - Analyze: Analyze data and identify gaps and inefficiency in current approach
+# MAGIC   - Improve: Present findings and get ready for improvement
+# MAGIC   - Control: Learn from work you did and put new process and documentation in place
+# MAGIC
+# MAGIC #### Project Management Methodologies and When to Choose Which 
+# MAGIC - [Link](https://www.teamwork.com/project-management-guide/project-management-methodologies/)
+# MAGIC #### Waterfall vs Agile
+# MAGIC
+# MAGIC |                        | Waterfall                                                                                                                                                                                 | Agile                                                                                                                                                                       |
+# MAGIC |------------------------|-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------|-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+# MAGIC | Project manager's role | Project manager serves as an active leader by prioritizing and assigning tasks to team members.                                                                                           | Agile project manager (or Scrum Master) acts primarily as a facilitator, removing any barriers the team faces.  Team shares more responsibility in managing their own work. |
+# MAGIC | Scope                  | Project deliverables and plans are well-established and documented in the early stages of initiating and planning.  Changes go through a formal change request process.                   | Planning happens in shorter iterations and focuses on delivering value quickly.  Subsequent iterations are adjusted in response to feedback or unforeseen issues.           |
+# MAGIC | Schedule               | Follows a mostly linear path through the initiating, planning, executing, and closing phases of the project.                                                                              | Time is organized into phases called Sprints. Each Sprint has a defined duration, with a set list of deliverables planned at the start of the Sprint.                       |
+# MAGIC | Cost                   | Costs are kept under control by careful estimation up front and close monitoring throughout the life cycle of the project.                                                                | Costs and schedule could change with each iteration.                                                                                                                        |
+# MAGIC | Quality                | Project manager makes plans and clearly defines criteria to measure quality at the beginning of the project.                                                                              | Team solicits ongoing stakeholder input and user feedback by testing products in the field and regularly implementing improvements.                                         |
+# MAGIC | Communication          | Project manager continually communicates progress toward milestones and other key indicators to stakeholders, ensuring that the project is on track to meet the customer’s expectations.  | Team is customer-focused, with consistent communication between users and the project team.                                                                                 |
+# MAGIC | Stakeholders           | Project manager continually manages and monitors stakeholder engagement to ensure the project is on track.                                                                                | Team frequently provides deliverables to stakeholders throughout the project. Progress toward milestones is dependent upon stakeholder feedback.                            |
